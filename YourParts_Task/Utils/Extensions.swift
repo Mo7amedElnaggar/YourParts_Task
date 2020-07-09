d51e5eec652dc38d1380dc7a44ae7c440538d9f1
Mo7amedElnaggar <iNagggar@gmail.com>
2020-07-09 22:09:39 +0200

    Allow Arbitrary Loads

---
+++ YourParts_Task/Utils/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  YourParts_Task
+//
+//  Created by Mohamed El-Naggar on 7/9/20.
+//  Copyright © 2020 Mohamed El-Naggar. All rights reserved.
+//
+
+import Foundation
---
+++ YourParts_Task/View/ExploreCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreCell.swift
+//  YourParts_Task
+//
+//  Created by Mohamed El-Naggar on 7/9/20.
+//  Copyright © 2020 Mohamed El-Naggar. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
